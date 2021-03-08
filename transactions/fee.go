0bb8b7cb4cc2ebfdf2d66f9b02949280175863cc
jojoin <yangjie@jojoin.com>
2021-03-08 17:19:32 +0800

    fix FeePurity Use Size / 8 byte

--- transactions/fee.go
+++ transactions/fee.go
@@ -6,12 +6,15 @@ import (
 )
 
 const (
-	FeePurityUnit = 232                          // = 0.00000001铢 = 1烁
-	MaxFeePurity  = uint64(18446744073709550000) // = 约1844枚
+	FeePurityUnit = 232                          // = 手续费含量单位 0.00000001铢 = 1烁
+	MaxFeePurity  = uint64(18446744073709550000) // = uint64的最大值 约1844枚
 
 )
 
-// 手续费含量 每byte的含有多少烁代币
+// 手续费含量 每 8 byte 的含有多少烁代币
+// 为何要按 8 个 byte 计算？
+// 因为可以避免手续费更多，但是由于手续费字段占用更多空间的问题，导致结构相同的交易
+// （比如创建钻石的交易）手续费更高但却排在了后面
 func CalculateFeePurity(trsFee *fields.Amount, txsize uint32) uint64 {
 	if trsFee.IsPositive() != true {
 		return 0
@@ -22,7 +25,11 @@ func CalculateFeePurity(trsFee *fields.Amount, txsize uint32) uint64 {
 	feeobj := trsFee.Copy()
 	feeobj.Unit -= FeePurityUnit
 	bigfee := feeobj.GetValue()
-	bigfee = bigfee.Div(bigfee, new(big.Int).SetUint64(uint64(txsize)))
+	calsize := uint64(txsize) / 8 // 每 8 个 byte 的手续费含量
+	if calsize == 0 {
+		calsize = 1 // 避免除0
+	}
+	bigfee = bigfee.Div(bigfee, new(big.Int).SetUint64(calsize))
 	if bigfee.Cmp(new(big.Int).SetUint64(MaxFeePurity)) == 1 {
 		return MaxFeePurity // max
 	} else {
